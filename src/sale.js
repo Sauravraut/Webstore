e96ded741e5adec92411198c1a3eee6780eed54c
Sauravraut <saurav2raut@gmail.com>
2021-09-05 12:20:23 +0200

    small changes

--- src/sale.js
+++ src/sale.js
@@ -1,4 +1,5 @@
-import "./sale.css"
+import "./sale.css";
+import flower from './img/flower.png';
 function Sale(){
     return(
         <div id = "body_mid">
@@ -6,21 +7,29 @@ function Sale(){
             <div id = "body_mid-main">
                 <div id ="body_mid-main-card_cont">
                     <div id = "card_holder">
-                        <div id = "card-1">  
+                        <div id = "card-1-cont">
+                            <div id = "card-1"></div> 
                         </div>
-                        <div id = "card-2">
+                        <div id = "card-2-cont">
+                            <div id = "card-2"></div> 
                         </div>
-                        <div id = "card-3">
+                        <div id = "card-3-cont">
+                         <div id = "card-3"></div> 
                         </div>
-                        <div id = "card-4">
+                        <div id = "card-4-cont">
+                            <div id = "card-4"></div> 
                         </div>
-                        <div id = "card-5">
+                        <div id = "card-5-cont">
+                            <div id = "card-5"></div> 
                         </div>
                     </div>
                 </div>
                 <div id ="body_mid-main-side">
                     <div id = "body_mid-title">
                             <p>SUMMER SALE</p>
+                    </div>
+                    <div id ="body_mid-gif">
+                        <img src = {flower} alt = "flower"></img>
                     </div>
                 </div>
             </div>
