1cb2bb6868879028389969fa796c3ba77e8a264d
Sauravraut <saurav2raut@gmail.com>
2021-09-07 11:17:18 +0200

    not much tbh

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,20 @@ function Bot(){
     return(
         <div id = "nav_bot">
             <div className = "gap"></div>
-            <div id = "nav_bot-main"></div>
+            <div id = "nav_bot-main">
+                <div>
+                <ul id = "nav_bot-main-1">
+                    <li className = "nav_bot-main-title">Products</li>
+                    <li>Mango+</li>
+                    <li>MangoTV</li>
+                    <li>EarPods</li>
+                    <li>Music</li>
+                    <li>Mpad</li>
+                    <li>Mtouch</li>
+                </ul>
+            </div>
         </div>
+    </div>
     )
 }
 export default Bot;
